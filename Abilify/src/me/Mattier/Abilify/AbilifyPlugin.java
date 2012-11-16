a6db35a0b5660eab3a7a67f84881441bb902e276
DurinKaine <halfbloodpokemon@gmail.com>
2012-11-16 14:52:18 -0500

    Began work on the AbilifyManager in the few minutes before leaving work.

--- Abilify/src/me/Mattier/Abilify/AbilifyPlugin.java
+++ Abilify/src/me/Mattier/Abilify/AbilifyPlugin.java
@@ -25,32 +25,30 @@ import org.spout.api.UnsafeMethod;
 import org.spout.api.plugin.CommonPlugin;
 
 public class AbilifyPlugin extends CommonPlugin {
-	private static Logger logger;
+	private static Logger LOGGER;
+	private static AbilifyManager MANAGER;
 	
-	@Override
-	@UnsafeMethod
+	@Override @UnsafeMethod
 	public void onDisable() {
+		MANAGER.stop();
 		info("Abilify: Disabled!");
 	}
 
-	@Override
-	@UnsafeMethod
+	@Override @UnsafeMethod
 	public void onEnable() {
-		logger = getLogger();
-		
+		LOGGER = getLogger();
+		MANAGER.start();
 		info("Abilify: Enabled!");
-		
-		//Spout.getPluginManager().loadPlugins(File arg0);
 	}
 	
-	@Override @UnsafeMethod public void onLoad() {}
+	@Override @UnsafeMethod public void onLoad() {
+		MANAGER = new AbilifyManager(this);
+	}
 	@Override @UnsafeMethod public void onReload() {}
 	
-	
-	
 /* Logger */
-	public static void info(String s) {logger.info(s);}
-	public static void warning(String s) {logger.warning(s);}
-	public static void severe(String s) {logger.severe(s);}
+	public static void info(String s) {LOGGER.info(s);}
+	public static void warning(String s) {LOGGER.warning(s);}
+	public static void severe(String s) {LOGGER.severe(s);}
 
 }
